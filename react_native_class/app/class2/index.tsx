b0c35f4c318d6cda3c9854c457db764ee156b395
Yareda21 <135129440+Yareda21@users.noreply.github.com>
2025-05-28 21:53:43 +0300

    updated

---
+++ react_native_class/app/class2/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+    return (
+        <View>
+            <Text>this is class 2</Text>
+        </View>
+    );
+};
+
+export default index;
