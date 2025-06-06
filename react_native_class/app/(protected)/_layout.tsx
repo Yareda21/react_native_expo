24674ade0a51f6ac6b5ccdec903feb81d60e75c7
Yareda21 <135129440+Yareda21@users.noreply.github.com>
2025-06-06 23:11:51 +0300

    updates

--- react_native_class/app/(protected)/_layout.tsx
+++ react_native_class/app/(protected)/_layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Tabs } from "expo-router";
 import { Colors } from "@/constants/colors";
 import { useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 const TabLayout = () => {
     const themeColor = useColorScheme();
     const theme = Colors[themeColor ?? "light"];
@@ -18,9 +19,57 @@ const TabLayout = () => {
                 tabBarInactiveTintColor: theme.iconColor,
             }}
         >
-            <Tabs.Screen name="Books" options={{ title: "Books" }} />
-            <Tabs.Screen name="Create" options={{ title: "Create" }} />
-            <Tabs.Screen name="dashboard" options={{ title: "Profile" }} />
+            <Tabs.Screen
+                name="Books"
+                options={{
+                    title: "Books",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "book" : "book-outline"}
+                            color={
+                                focused
+                                    ? theme.iconColorFocused
+                                    : theme.iconColor
+                            }
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Create"
+                options={{
+                    title: "Create",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "create" : "create-outline"}
+                            color={
+                                focused
+                                    ? theme.iconColorFocused
+                                    : theme.iconColor
+                            }
+                        />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="dashboard"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons
+                            size={24}
+                            name={focused ? "person" : "person-outline"}
+                            color={
+                                focused
+                                    ? theme.iconColorFocused
+                                    : theme.iconColor
+                            }
+                        />
+                    ),
+                }}
+            />
         </Tabs>
     );
 };
